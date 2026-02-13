5f83a4143b91920e92d1fae33303bedbd12a4d07
Federico Cantore <f.cantore85@gmail.com>
2026-02-13 18:40:35 +0100

    refactor: change create() type from T to void

--- src/main/java/common/persistance/Repository.java
+++ src/main/java/common/persistance/Repository.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 
 public interface Repository<T, ID> {
 
-    T create(T entity);
+    void create(T entity);
     Optional<T> getById(ID id);
     List<T> getAll();
     void modify(T entity);
