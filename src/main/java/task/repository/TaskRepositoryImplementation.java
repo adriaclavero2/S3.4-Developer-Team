77143d7c4f220400660fbe005e0f709379411be6
Federico Cantore <f.cantore85@gmail.com>
2026-02-13 18:03:34 +0100

    feat: implemented TaskRepository interface to TaskRepositoryImplemantation

--- src/main/java/task/repository/TaskRepositoryImplementation.java
+++ src/main/java/task/repository/TaskRepositoryImplementation.java
@@ -1,11 +1,45 @@
 package task.repository;
 
 import common.persistance.TaskDAO;
+import task.model.Task;
 
-public class TaskRepositoryImplementation {
+import java.util.List;
+import java.util.Optional;
+
+public class TaskRepositoryImplementation implements TaskRepository{
     private final TaskDAO dao;
 
     public TaskRepositoryImplementation(TaskDAO dao) {
         this.dao = dao;
+    }
+
+    @Override
+    public Task create(Task entity) {
+        return null;
+    }
+
+    @Override
+    public Optional<Task> getById(String s) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Task> getAll() {
+        return List.of();
+    }
+
+    @Override
+    public void modify(Task entity) {
+
+    }
+
+    @Override
+    public void remove(String s) {
+
+    }
+
+    @Override
+    public List<Task> getCompletedTasks() {
+        return List.of();
     }
 }
