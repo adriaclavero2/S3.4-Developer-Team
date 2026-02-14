512d35104a971a9fd6514475fb37a903d601b7d6
Federico Cantore <f.cantore85@gmail.com>
2026-02-14 11:42:08 +0100

    feat: add MongoBuilder in common.config.builders

---
+++ src/main/java/common/config/builders/MongoBuilder.java
@@ -0,0 +1,15 @@
+package common.config.builders;
+
+import common.config.ConnectionStringBuilder;
+import java.util.Properties;
+
+public class MongoBuilder implements ConnectionStringBuilder {
+    @Override
+    public String stringBuild(Properties props) {
+        return String.format("mongodb://%s:%s@%s:%s",
+                props.getProperty("MONGO_USER"),
+                props.getProperty("MONGO_PASSWORD"),
+                props.getProperty("MONGO_HOST"),
+                props.getProperty("MONGO_PORT"));
+    }
+}
