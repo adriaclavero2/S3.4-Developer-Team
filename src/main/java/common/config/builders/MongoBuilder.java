167683a4236075b99ab3d9d5aef57e41a6476fb2
federicopratico <federico.pratico@icloud.com>
2026-02-15 18:12:43 +0100

    fix: change variable names in getPropery() methods

--- src/main/java/common/config/builders/MongoBuilder.java
+++ src/main/java/common/config/builders/MongoBuilder.java
@@ -7,14 +7,14 @@ public class MongoBuilder implements ConnectionStringBuilder {
     @Override
     public String stringBuild(Properties props) {
         return String.format("mongodb://%s:%s@%s:%s",
-                props.getProperty("MONGO_USER"),
-                props.getProperty("MONGO_PASSWORD"),
-                props.getProperty("MONGO_HOST"),
-                props.getProperty("MONGO_PORT"));
+                props.getProperty("mongodb.username"),
+                props.getProperty("mongodb.password"),
+                props.getProperty("mongodb.host"),
+                props.getProperty("mongodb.port"));
     }
 
     @Override
     public String getDbName(Properties props) {
-        return props.getProperty("MONGO_DB");
+        return props.getProperty("mongodb.database");
     }
 }
