ea49a1c6e1d114cc76a9d42853ca7494ad1cf40f
federicopratico <federico.pratico@icloud.com>
2026-02-15 18:16:42 +0100

    feature: createTask() method added

--- src/main/java/task/service/TaskService.java
+++ src/main/java/task/service/TaskService.java
@@ -1,4 +1,27 @@
 package task.service;
 
+import com.mongodb.client.MongoDatabase;
+import common.persistance.Repository;
+import infrastructure.mongo.dao.MongoTaskDAOAdapter;
+import task.model.Task;
+import task.model.TaskDTO;
+import task.repository.TaskRepository;
+import task.repository.TaskRepositoryImplementation;
+
 public class TaskService {
+    private TaskRepository repository;
+
+    public TaskService(MongoDatabase database) {
+        this.repository = new TaskRepositoryImplementation(
+                new MongoTaskDAOAdapter(database.getCollection("tasks")));
+    }
+
+    public void createTask(Task newTask) {
+        try {
+            repository.create(newTask);
+            System.out.println("New task " + newTask.getTitle() + " created");
+        } catch (RuntimeException e) {
+            System.err.println(e.getMessage());
+        }
+    }
 }
