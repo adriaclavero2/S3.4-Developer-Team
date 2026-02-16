d7c0c0f8254cba2fecb7e5067005f1a9b1d7d901
Federico Cantore <f.cantore85@gmail.com>
2026-02-16 16:42:06 +0100

    fix: changed method type to Mongo Document
    feat: changed method seve()

--- src/main/java/infrastructure/mongo/dao/MongoTaskDAOAdapter.java
+++ src/main/java/infrastructure/mongo/dao/MongoTaskDAOAdapter.java
@@ -7,6 +7,7 @@ import common.exception.DataAccessException;
 import common.persistance.TaskDAO;
 import infrastructure.mongo.connection.MongoDBConnection;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 import task.model.Task;
 import task.model.TaskCopyBuilder;
 
@@ -24,26 +25,26 @@ public class MongoTaskDAOAdapter implements TaskDAO {
     }
 
     @Override
-    public void save(Task entity) {
+    public void save(Document doc) {
         try {
-            Document doc = new Document()
+           /* Document doc = new Document()
                     .append("title", entity.getTitle())
                     .append("description", entity.getDescription())
                     .append("expiredAt", entity.getExpireDate() != null ? entity.getExpireDate().toString() : null)
                     .append("priority", entity.getPriority().name())
                     .append("status", entity.getTaskState().name())
-                    .append("createdAt", entity.getCreationDate().toString());
+                    .append("createdAt", entity.getCreationDate().toString());*/
 
             collection.insertOne(doc);
 
-            String generatedId = doc.getObjectId("_id").toHexString();
+            //String generatedId = doc.getObjectId("_id").toHexString();
 
-            entity.setId(generatedId);
+           /*entity.setId(generatedId);
 
            Task taskWithId = new TaskCopyBuilder()
                         .copyOf(entity)
                         .setId(generatedId)
-                        .build();
+                        .build();*/
 
         } catch (MongoException e) {
             throw new DataAccessException("MongoDB", e);
@@ -52,28 +53,28 @@ public class MongoTaskDAOAdapter implements TaskDAO {
     }
 
     @Override
-    public Optional<Task> findByID(String id) {
+    public Optional<Document> findByID(String id) {
         return Optional.empty();
     }
 
     @Override
-    public List<Task> findAll() {
+    public List<Document> findAll() {
         return List.of();
     }
 
     @Override
-    public void update(Task entity) {
+    public void update(Document doc) {
         try {
-            Document query = new Document("_id", new org.bson.types.ObjectId(entity.getId()));/*Metodo de Mongo que toma el String hexadecimal y lo convierte en binario  */
+           /*Document query = new Document("_id", new org.bson.types.ObjectId(entity.getId()));/*Metodo de Mongo que toma el String hexadecimal y lo convierte en binario
 
             Document updates = new Document()
                     .append("title", entity.getTitle())
                     .append("description", entity.getDescription())
                     .append("expiredAt", entity.getExpireDate() != null ? entity.getExpireDate().toString() : null)
                     .append("priority", entity.getPriority().name())
-                    .append("status", entity.getTaskState().name());
+                    .append("status", entity.getTaskState().name());*/
 
-            collection.updateOne(query, new Document("$set", updates)); /*Metodo de Mongo para update. En new Document "$set" lo va a interpretar como la funcion set de mongo y modifica el elemento en el server. las key que comienzan con $ se interpretan como operadores de accion*/
+            collection.updateOne(doc.get("_id"), new Document("$set", doc)); /*Metodo de Mongo para update. En new Document "$set" lo va a interpretar como la funcion set de mongo y modifica el elemento en el server. las key que comienzan con $ se interpretan como operadores de accion*/
         } catch (Exception e) {
             throw new DataAccessException("MongoDB update", e);
         }
