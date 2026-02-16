3b8a0606aa8acedfa491899619c23b513689efe6
federicopratico <federico.pratico@icloud.com>
2026-02-16 11:45:24 +0100

    added TaskMapper implementation of GenericMapper

---
+++ src/main/java/task/mapper/TaskMapper.java
@@ -0,0 +1,48 @@
+package task.mapper;
+
+import common.utils.GenericMapper;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import task.enums.Priority;
+import task.enums.TaskState;
+import task.model.Task;
+import task.model.TaskBuilder;
+
+import java.time.LocalDateTime;
+
+public class TaskMapper implements GenericMapper <Task, Document> {
+    @Override
+    public Document toDocument(Task task) {
+        return new Document()
+                .append("title", task.getTitle())
+                .append("description", task.getDescription())
+                .append("expiredAt", task.getExpireDate() != null ? task.getExpireDate().toString() : null)
+                .append("priority", task.getPriority().name())
+                .append("status", task.getTaskState().name())
+                .append("createdAt", task.getCreationDate().toString());
+
+    }
+
+    @Override
+    public Task toDomain(Document document) {
+
+        LocalDateTime expDate = document.getString("expiredAt") != null ? LocalDateTime.parse(document.getString("expiredAt")) : null;
+        LocalDateTime creationDAte = LocalDateTime.parse(document.getString("createdAt"));
+        ObjectId objID = document.getObjectId("_id");
+
+        Task task = TaskBuilder.newTask()
+                .title(document.getString("title"))
+                .description(document.getString("description"))
+                .expireDate(expDate)
+                .taskState(TaskState.valueOf(document.getString("status")))
+                .priority(Priority.valueOf(document.getString("priority")))
+                .build();
+
+        task.setId(objID.toHexString());
+        task.setCreationDate(creationDAte);
+
+
+        return task;
+    }
+
+}
