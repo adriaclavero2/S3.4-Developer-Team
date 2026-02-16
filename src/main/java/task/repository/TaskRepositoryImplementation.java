6f52468a2def8663ce9b71964c2b192aea1a426d
Federico Cantore <f.cantore85@gmail.com>
2026-02-16 16:43:58 +0100

    feat: add GenericMapper to TaskRepositoryImplementation

--- src/main/java/task/repository/TaskRepositoryImplementation.java
+++ src/main/java/task/repository/TaskRepositoryImplementation.java
@@ -2,13 +2,26 @@ package task.repository;
 
 import common.exception.DataAccessException;
 import common.persistance.TaskDAO;
+import common.utils.GenericMapper;
+import org.bson.Document;
 import task.model.Task;
-
 import java.util.List;
 import java.util.Optional;
 
 public class TaskRepositoryImplementation implements TaskRepository{
+
     private final TaskDAO taskDAO;
+    private final GenericMapper mapper = new GenericMapper() {
+        @Override
+        public Object toDocument(Object o) {
+            return null;
+        }
+
+        @Override
+        public Object toDomain(Object o) {
+            return null;
+        }
+    };
 
     public TaskRepositoryImplementation(TaskDAO taskDAO) {
         this.taskDAO = taskDAO;
@@ -17,7 +30,8 @@ public class TaskRepositoryImplementation implements TaskRepository{
     @Override
     public void create(Task entity) {
         try {
-            taskDAO.save(entity);
+            Document doc = mapper.toDocument(entity);
+            taskDAO.save(doc);
             System.out.println("Log: task created successfully");
         } catch (DataAccessException e) {
             throw new DataAccessException("MongoDB" + e);
@@ -31,13 +45,20 @@ public class TaskRepositoryImplementation implements TaskRepository{
 
     @Override
     public List<Task> getAll() {
+        try {
+            taskDAO.findAll();
+            System.out.println("Log: task list all successfully");
+        } catch (DataAccessException e) {
+            throw new DataAccessException("MongoDB", e);
+        }
         return List.of();
     }
 
     @Override
     public void modify(Task entity) {
         try {
-            taskDAO.update(entity);
+            Document doc = mapper.toDocument(entity);
+            taskDAO.update(doc);
             System.out.println("Log: task updated successfully");
         } catch (DataAccessException e) {
             throw new DataAccessException("Error modifying task: " + e.getMessage());
