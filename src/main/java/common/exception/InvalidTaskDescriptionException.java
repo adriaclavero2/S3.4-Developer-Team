ab38a4fa4243d97233f9c187433562edb1a356e5
federicopratico <federico.pratico@icloud.com>
2026-02-18 12:01:48 +0100

    added constant variables in InvalidTaskDescriptionException

--- src/main/java/common/exception/InvalidTaskDescriptionException.java
+++ src/main/java/common/exception/InvalidTaskDescriptionException.java
@@ -1,7 +1,10 @@
 package common.exception;
 
 public class InvalidTaskDescriptionException extends RuntimeException {
-  public InvalidTaskDescriptionException(String message) {
-    super(message);
-  }
+    public static final String NULL_DESCRIPTION_ERROR = "Task description cannot be null.";
+    public static final String EMPTY_DESCRIPTION_ERROR = "Task description cannot be empty.";
+
+    public InvalidTaskDescriptionException(String message) {
+        super(message);
+    }
 }
