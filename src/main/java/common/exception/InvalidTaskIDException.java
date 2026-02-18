432b366003af4795b84f073ee8ef54cea074bc3b
federicopratico <federico.pratico@icloud.com>
2026-02-18 20:37:12 +0100

    added new costructor

--- src/main/java/common/exception/InvalidTaskIDException.java
+++ src/main/java/common/exception/InvalidTaskIDException.java
@@ -4,4 +4,8 @@ public class InvalidTaskIDException extends RuntimeException {
     public InvalidTaskIDException(String message) {
         super(message);
     }
+
+    public InvalidTaskIDException(String message, Throwable exception) {
+        super(message,exception);
+    }
 }
