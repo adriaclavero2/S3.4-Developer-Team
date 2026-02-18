94f7b846183d8050df138274d0e11909da49bab0
Federico Cantore <f.cantore85@gmail.com>
2026-02-18 21:04:40 +0100

    feat: refactor mapper including _id in toDocument() to support updates

--- src/main/java/task/mapper/TaskMapper.java
+++ src/main/java/task/mapper/TaskMapper.java
@@ -13,13 +13,22 @@ import java.time.LocalDateTime;
 public class TaskMapper implements Mapper<Task, Document> {
     @Override
     public Document toDocument(Task task) {
-        return new Document()
+        Document doc = new Document()
                 .append("title", task.getTitle())
                 .append("description", task.getDescription())
                 .append("expiredAt", task.getExpireDate() != null ? task.getExpireDate().toString() : null)
                 .append("priority", task.getPriority().name())
                 .append("status", task.getTaskState().name())
                 .append("createdAt", task.getCreationDate().toString());
+
+        // Lógica para el ID (Crucial para el UPDATE):
+        // Si la Task ya tiene un ID (no es nulo ni está vacío), lo añadimos al Document.
+        // Esto permite que tu DAO encuentre el documento correcto en MongoDB.
+        if (task.getId() != null && !task.getId().isEmpty()) {
+            doc.append("_id", new ObjectId(task.getId()));
+        }
+
+        return doc;
 
     }
 
