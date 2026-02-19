1333ffc83a471298357d2f823c3a6f5754b519fc
federicopratico <federico.pratico@icloud.com>
2026-02-19 13:02:17 +0100

    added and deleted TaskrespoitoryImplDeleteTest

--- src/test/java/task/service/TaskServiceDeleteTaskTest.java
+++ src/test/java/task/service/TaskServiceDeleteTaskTest.java
@@ -1,4 +1,23 @@
 package task.service;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import task.repository.TaskRepository;
+
+@ExtendWith(MockitoExtension.class)
 public class TaskServiceDeleteTaskTest {
+
+    @Mock
+    private TaskRepository taskRepository;
+
+    private TaskService taskService;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+
 }
