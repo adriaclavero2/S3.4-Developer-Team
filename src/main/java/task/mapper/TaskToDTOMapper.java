777220b40d7440fc4abf3349dcd9d2f5fdd15ba0
federicopratico <federico.pratico@icloud.com>
2026-02-20 20:09:53 +0100

    created and implemented mapper to map domain object to the external UI layer and vice versa

--- src/main/java/task/mapper/TaskToDTOMapper.java
+++ src/main/java/task/mapper/TaskToDTOMapper.java
@@ -1,4 +1,45 @@
 package task.mapper;
 
+import task.dto.OutputTaskDTO;
+import task.dto.TaskDTO;
+import task.enums.Priority;
+import task.model.Task;
+import task.model.TaskBuilder;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class TaskToDTOMapper {
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+    private DateTimeFormatter onlyDateFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+    public Task dtoToTask(TaskDTO dto) {
+        LocalDateTime expireDate = !dto.expireDate().isEmpty() ? LocalDateTime.parse(dto.expireDate(), onlyDateFormatter) : null;
+
+        Task task = TaskBuilder.newTask()
+                .withTitle(dto.title())
+                .withDescription(dto.description())
+                .withExpireDate(expireDate)
+                .withPriority(Priority.valueOf(dto.priority().toUpperCase()))
+                .build();
+
+        return task;
+    }
+
+    public OutputTaskDTO taskToDto(Task task) {
+        String creationDate = task.getCreationDate().format(formatter);
+        String expireDate = task.getExpireDate() != null ? task.getCreationDate().format(formatter) : "undefined";
+
+
+        OutputTaskDTO output = new OutputTaskDTO(
+                task.getId(),
+                task.getTitle(),
+                task.getDescription(),
+                expireDate,
+                creationDate,
+                task.getPriority().toString(),
+                task.getTaskState().toString());
+
+        return output;
+    }
 }
