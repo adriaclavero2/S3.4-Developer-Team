517ced425767159b22e90841693910c9eb08f52b
Federico Cantore <f.cantore85@gmail.com>
2026-02-24 10:07:21 +0100

    refactor: change to printFoundTask() in getTaskById

--- src/main/java/application/menu/TaskMenu.java
+++ src/main/java/application/menu/TaskMenu.java
@@ -145,7 +145,7 @@ public class TaskMenu {
         OutputDTO result = taskService.getTaskById(dto);
 
         if (result instanceof  OutputTaskDTO success) {
-            printCreatedTask( success);
+            printFoundTask( success);
         } else if (result instanceof  ErrorOutputDTO error) {
             System.err.println("Error: " + error.getOutputState());
         }
