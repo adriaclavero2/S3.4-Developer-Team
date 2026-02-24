d2f4ebf13c519d70d436c5fb0d88746e0fd9f61e
federicopratico <federico.pratico@icloud.com>
2026-02-24 11:29:15 +0100

    change print function for updated task

--- src/main/java/common/utils/MenuPrinter.java
+++ src/main/java/common/utils/MenuPrinter.java
@@ -56,7 +56,7 @@ public class MenuPrinter {
         System.out.println("| Title: " + dto.title() +                        "|");
         System.out.println("| Description: " + dto.description() +            "|");
         System.out.println("| Expire date " + expireDate +                    "|");
-        System.out.println("| Priority: " + dto.id() +                        "|");
+        System.out.println("| Priority: " + dto.priority() +                        "|");
         System.out.println("| State: " + dto.taskState() +                    "|");
         System.out.println("|==================================================|");
     }
